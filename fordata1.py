9bb72dfc44b11b91910d77e2ec98dafaca9db074
Polqer <polina.delitzsch@gmail.com>
2024-11-23 13:37:42 +0300

    New changes

--- fordata1.py
+++ fordata1.py
@@ -22,86 +22,115 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 torch.backends.cudnn.benchmark=True
 gc.collect()
 drive.mount('/content/drive')
-# Пути к данным на Google Диске
-data_dir = '/content/drive/MyDrive/Dataset1'
+#пути
+data_dir = '/content/drive/MyDrive/dataset_split'
 result_dir = '/content/drive/MyDrive/Results'
+train_dir = os.path.join(data_dir, 'train')
+val_dir = os.path.join(data_dir, 'val')
+test_dir = os.path.join(data_dir, 'test')
 torch.cuda.empty_cache()
 
-batch_sz = 16
+torch.backends.cuda.matmul.allow_tf32 = True
+
+
+batch_sz = 8
 num_epoch = 20
 init_learning_rate = 0.0001
 learning_rate_decay_factor = 0.2
 num_epochs_decay = 15
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Using device: {device}")
+if device.type == "cuda":
+    print(f"GPU: {torch.cuda.get_device_name(0)}")
 mean = np.array([0.485, 0.456, 0.406])
 std = np.array([0.229, 0.224, 0.225])
 
-data_transforms = {'train': transforms.Compose([
+# data_transforms = {'train': transforms.Compose([
+#         transforms.RandomHorizontalFlip(),
+#         transforms.Resize(256),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean, std)]),
+#     'val': transforms.Compose([
+#         transforms.Resize(256),
+#         transforms.ToTensor(),
+#         transforms.Normalize(mean, std)]),}
+
+data_transforms = {
+    'train': transforms.Compose([
         transforms.RandomResizedCrop(224),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize(mean, std)]),
+        transforms.Normalize(mean, std)
+    ]),
     'val': transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
-        transforms.Normalize(mean, std)]),}
-
-def prepare_datasets(data_dir, train_split=0.7, val_split=0.15, test_split=0.15):
-    # Загружаем полный датасет
-    full_dataset = datasets.ImageFolder(data_dir, transform=data_transforms['train'])
-
-    # Делим на тренировочную и оставшуюся выборку (валидация + тест)
-    train_size = int(train_split * len(full_dataset))
-    remaining_size = len(full_dataset) - train_size
-
-    # Оставшуюся выборку делим на валидацию и тест
-    val_size = int(val_split * remaining_size)
-    test_size = remaining_size - val_size
-
-    # Разделение на три части
-    train_dataset, remaining_dataset = random_split(full_dataset, [train_size, remaining_size])
-    val_dataset, test_dataset = random_split(remaining_dataset, [val_size, test_size])
-
-    # Применяем соответствующие трансформации
-    train_dataset.dataset.transform = data_transforms['train']
-    val_dataset.dataset.transform = data_transforms['val']
-    test_dataset.dataset.transform = data_transforms['val']
-
-    return train_dataset, val_dataset, test_dataset
-
-train_dataset, val_dataset, test_dataset = prepare_datasets(data_dir)
-
-# Создаём загрузчики данных
-dataloaders = {
-    'train': torch.utils.data.DataLoader(train_dataset, batch_size=batch_sz, shuffle=True, num_workers=2),
-    'val': torch.utils.data.DataLoader(val_dataset, batch_size=batch_sz, shuffle=True, num_workers=2),
-    'test': torch.utils.data.DataLoader(test_dataset, batch_size=batch_sz, shuffle=False, num_workers=2)
+        transforms.Normalize(mean, std)
+    ]),
 }
+
+# image_datasets = {'train': datasets.ImageFolder(train_dir, data_transforms['train']),
+#                               'val': datasets.ImageFolder(test_dir, data_transforms['val'])}
+# dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=batch_sz,
+#                                               shuffle=True, num_workers=4)
+#                for x in ['train', 'val']}
+# dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+# class_names = image_datasets['train'].classes
+# class_num = len(class_names)
+
+#данные из папок
+
+train_dataset = datasets.ImageFolder(train_dir, transform=data_transforms['train'])
+val_dataset = datasets.ImageFolder(val_dir, transform=data_transforms['val'])
+
+dataloaders = {
+    'train': torch.utils.data.DataLoader(train_dataset, batch_size=batch_sz, shuffle=True),
+    'val': torch.utils.data.DataLoader(val_dataset, batch_size=batch_sz, shuffle=True)
+}
+
+
 
 dataset_sizes = {
     'train': len(train_dataset),
-    'val': len(val_dataset),
-    'test': len(test_dataset)
-}
-class_names = train_dataset.dataset.classes
+    'val': len(val_dataset)}
+class_names = train_dataset.classes
 class_num = len(class_names)
-#исходный объект ImageFolder
-full_dataset = val_dataset.dataset.dataset
-# индексы классов в val_dataset
-val_labels = [val_dataset[i][1] for i in range(len(val_dataset))]
-#считаем, сколько примеров каждого класса
-val_class_counts = Counter(val_labels)
-#количество примеров каждого класса
-print("Validation dataset class counts:")
-for cls_idx, count in val_class_counts.items():
-    print(f"Class {full_dataset.classes[cls_idx]}: {count}")
-#получаем индексы классов в train_dataset
-train_labels = [train_dataset[i][1] for i in range(len(train_dataset))]
-train_class_counts = Counter(train_labels)
-print("\nTraining dataset class counts:")
-for cls_idx1, count in train_class_counts.items():
-    print(f"Class {full_dataset.classes[cls_idx1]}: {count}")
+# #кол-во изображений для каждого класса
+# def count_class_samples(dataset, dataset_name):
+#     class_counts = Counter([sample[1] for sample in dataset])
+#     print(f"\n{dataset_name} dataset class counts:")
+#     for cls_idx, count in class_counts.items():
+#         print(f"Class {dataset.classes[cls_idx]}: {count}")
 
 
-print(f"Train size: {dataset_sizes['train']}, Validation size: {dataset_sizes['val']}, Test size: {dataset_sizes['test']}")
+# count_class_samples(train_dataset, "train")
+
+# count_class_samples(val_dataset, "validation")
+
+
+
+# #Ссылаемся на исходный объект ImageFolder
+# full_dataset = val_dataset.dataset.dataset
+
+# #Получаем индексы классов в val_dataset
+# val_labels = [val_dataset[i][1] for i in range(len(val_dataset))]
+# #Считаем, сколько примеров каждого класса
+# val_class_counts = Counter(val_labels)
+# # Выводим количество примеров каждого класса
+# print("Validation dataset class counts:")
+# for cls_idx, count in val_class_counts.items():
+#     print(f"Class {full_dataset.classes[cls_idx]}: {count}")
+
+
+# #Получаем индексы классов в train_dataset
+# train_labels = [train_dataset[i][1] for i in range(len(train_dataset))]
+# #Считаем, сколько примеров каждого класса
+# train_class_counts = Counter(train_labels)
+# #Выводим количество примеров каждого класса
+# print("\nTraining dataset class counts:")
+# for cls_idx1, count in train_class_counts.items():
+#     print(f"Class {full_dataset.classes[cls_idx1]}: {count}")
+
+
+#print(f"Train size: {dataset_sizes['train']}, Validation size: {dataset_sizes['val']}, Test size: {dataset_sizes['test']}")
--- step_1.py
+++ step_1.py
@@ -14,21 +14,20 @@ from sklearn.metrics import recall_score, f1_score, precision_score, accuracy_sc
 from PIL import ImageFile
 from google.colab import drive
 import gc
-import wandb
 from sklearn.preprocessing import label_binarize
+import wandb
 from fordata1 import *
-wandb.init(project="my-awesome-project", settings=wandb.Settings(init_timeout=300))
 
-
-# Создаем ансамблевую модель, ResNet152 + ViT
+wandb.init(project="my-awesome-project", name="step1_checki_2")
+#cоздаем ансамблевую модель, ResNet152 + ViT
 class EnsembleModel(nn.Module):
     def __init__(self, class_num):
         super(EnsembleModel, self).__init__()
-        # ResNet152
+        #ResNet152
         self.resnet = models.resnet152()  # Используем ResNet152
         num_ftrs_resnet = self.resnet.fc.in_features
         self.resnet.fc = nn.Identity()  # - последний слой, чтобы использовать его выходы
-        # ViT
+        #ViT
         self.vit = models.vit_b_16()
         num_ftrs_vit = self.vit.heads.head.in_features
         self.vit.heads.head = nn.Identity()  # аналогично
@@ -36,30 +35,82 @@ class EnsembleModel(nn.Module):
         self.classifier = nn.Linear(num_ftrs_resnet + num_ftrs_vit, class_num)
 
     def forward(self, x):
-        # Прямой проход через ResNet и ViT
+        #прямой проход через ResNet и ViT
         resnet_out = self.resnet(x)
         vit_out = self.vit(x)
-        # Сцепление выходов
+        #сцепление выходов
         combined = torch.cat((resnet_out, vit_out), dim=1)
-        # Итог предсказание
+        #итог предсказание
         out = self.classifier(combined)
         return out
 
-# Экземпляр ансамблевой модели
+#функция для сохранения чекпоинта
+def save_checkpoint(model, optimizer, scheduler, epoch, checkpoint_dir="/content/drive/MyDrive/Results"):
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    checkpoint_path = os.path.join(checkpoint_dir, f"checkpoint_epoch_{epoch + 1}.ckpt")
+    torch.save({
+        'epoch': epoch,
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'scheduler_state_dict': scheduler.state_dict(),
+    }, checkpoint_path)
+    print(f"Checkpoint saved at {checkpoint_path}")
+
+#функция для загрузки чекпоинта
+def load_checkpoint(model, optimizer, scheduler, checkpoint_dir="/content/drive/MyDrive/Result"):
+    checkpoints = [os.path.join(checkpoint_dir, f) for f in os.listdir(checkpoint_dir) if f.endswith('.ckpt')]
+    if not checkpoints:
+        print("No checkpoint found, starting from scratch.")
+        return model, optimizer, scheduler, 0  # Начать с нуля
+
+    #найти последний чекпоинт по названию
+    latest_checkpoint = max(checkpoints, key=os.path.getctime)
+    checkpoint = torch.load(latest_checkpoint)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    start_epoch = checkpoint['epoch'] + 1
+    print(f"Checkpoint loaded: {latest_checkpoint}, resuming from epoch {start_epoch}")
+    return model, optimizer, scheduler, start_epoch
+def calculate_metrics(y_true, y_pred, y_true_proba, y_pred_proba):
+    return {
+        'accuracy': accuracy_score(y_true=y_true, y_pred=y_pred),
+        'confusion_matrix': confusion_matrix(y_true=y_true, y_pred=y_pred),
+        'micro/precision': precision_score(y_true=y_true, y_pred=y_pred, average='micro', zero_division=0),
+        'micro/recall': recall_score(y_true=y_true, y_pred=y_pred, average='micro', zero_division=0),
+        'micro/f1': f1_score(y_true=y_true, y_pred=y_pred, average='micro', zero_division=0),
+        'micro/roc_auc_score': roc_auc_score(y_true=y_true_proba, y_score=y_pred_proba, average='micro'),
+        'macro/precision': precision_score(y_true=y_true, y_pred=y_pred, average='macro', zero_division=0),
+        'macro/recall': recall_score(y_true=y_true, y_pred=y_pred, average='macro', zero_division=0),
+        'macro/f1': f1_score(y_true=y_true, y_pred=y_pred, average='macro', zero_division=0),
+        'roc_auc_score': roc_auc_score(y_true=y_true_proba, y_score=y_pred_proba, average='samples'),
+        'weighted/precision': precision_score(y_true=y_true, y_pred=y_pred, average='weighted', zero_division=0),
+        'weighted/recall': recall_score(y_true=y_true, y_pred=y_pred, average='weighted', zero_division=0),
+        'weighted/f1': f1_score(y_true=y_true, y_pred=y_pred, average='weighted', zero_division=0)
+    }
+#используем
+checkpoint_dir = "/content/drive/MyDrive/Result"
+
+
+#экземпляр ансамблевой модели
 model_ft = EnsembleModel(class_num).to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=init_learning_rate)
-# поменяла ReduceLROnPlateau
+#поменяла ReduceLROnPlateau
 exp_lr_scheduler = lr_scheduler.ReduceLROnPlateau(optimizer_ft, mode='min', factor=learning_rate_decay_factor, patience=10)
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
+#загрузка чекпоинта, если он существует
+model_ft, optimizer_ft, exp_lr_scheduler, start_epoch = load_checkpoint(model_ft, optimizer_ft, exp_lr_scheduler, checkpoint_dir)
+
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch, start_epoch=0):
     since = time.time()
     best_model_wts = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-    # Хранение истории обучения
+    #хранение истории обучения
     train_losses, val_losses = [], []
     train_accuracies, val_accuracies = [], []
-    for epoch in range(num_epochs):
+    for epoch in range(start_epoch, num_epochs):
         print('Epoch {}/{}'.format(epoch, num_epochs - 1))
         print('-' * 10)
 
@@ -73,11 +124,15 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
             running_loss = 0.0
             running_corrects = 0
 
-            # Массивы для подсчета метрик по классам
+            #массивы для подсчета метрик по классам
             all_labels = []
             all_preds = []
-
-            # Обработка данных
+            all_true_proba = []
+            all_pred_proba = []
+            print(len(dataloaders[phase].dataset))  # Убедитесь, что есть данные
+            print("Train set length:", len(dataloaders['train'].dataset))
+            print("Validation set length:", len(dataloaders['val'].dataset))
+            #обработка данных
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
@@ -87,7 +142,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
                     outputs = model(inputs)
                     _, preds = torch.max(outputs, 1)
                     loss = criterion(outputs, labels)
-
+#сохраняем вероятности для ROC-AUC
+                    probabilities = torch.softmax(outputs, dim=1).tensor.detach().numpy()
                     if phase == 'train':
                         loss.backward()
                         optimizer.step()
@@ -97,85 +153,68 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
 
                 all_labels.extend(labels.cpu().numpy())
                 all_preds.extend(preds.cpu().numpy())
-
-        # Расчет метрик для эпохи
+                all_true_proba.extend(np.eye(class_num)[labels.cpu().numpy()])
+                all_pred_proba.extend(probabilities)
+            #расчет метрик для эпохи
             epoch_loss = running_loss / dataset_sizes[phase]
             epoch_acc = running_corrects.double() / dataset_sizes[phase]
 
-        # Сохранение значений потерь и точности для графиков
+            #сохранение значений потерь и точности для графиков
             if phase == 'train':
                 train_losses.append(epoch_loss)
-                train_accuracies.append(epoch_acc)
+                train_accuracies.append(epoch_acc.item() if isinstance(epoch_acc, torch.Tensor) else epoch_acc)
             else:
                 val_losses.append(epoch_loss)
-                val_accuracies.append(epoch_acc)
+                val_accuracies.append(epoch_acc.item() if isinstance(epoch_acc, torch.Tensor) else epoch_acc)
+
+
+
+
+
 
             print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
 
-            # # Расчет чувствительности и специфичности для каждого класса
-            # if phase == 'val':
-            #     cm = confusion_matrix(all_labels, all_preds)
-            #     sensitivity = cm.diagonal() / cm.sum(axis=1)
-            #     specificity = (cm.sum() - cm.sum(axis=0) - cm.sum(axis=1) + cm.diagonal()) / (cm.sum() - cm.sum(axis=1))
-            #     print('Sensitivity:', sensitivity)
-            #     print('Specificity:', specificity)
-            # Проверяем валидные строки и столбцы перед расчетами
+
+
             if phase == 'val':
-              cm = confusion_matrix(all_labels, all_preds)
-              sensitivity = []
-              specificity = []
-              num_classes = cm.shape[0]  # Число классов
+                #расчет дополнительных метрик
+                metrics = calculate_metrics(
+                    y_true=all_labels,
+                    y_pred=all_preds,
+                    y_true_proba=np.array(all_true_proba),
+                    y_pred_proba=np.array(all_pred_proba)
+                )
+                print("\nValidation metrics:")
+                for key, value in metrics.items():
+                    if key != 'confusion_matrix':
+                        print(f"{key}: {value:.4f}")
 
-              for i in range(num_classes):
-                  TP = cm[i, i]
-                  FN = cm[i, :].sum() - TP
-                  FP = cm[:, i].sum() - TP
-                  TN = cm.sum() - (TP + FN + FP)
 
-    # Чувствительность (Sensitivity)
-                  sensitivity_class = TP / (TP + FN) if (TP + FN) > 0 else 0
-                  sensitivity.append(sensitivity_class)
+                #лоогирунм метрики в W&B
+                wandb.log({
+                    "Val Loss": epoch_loss,
+                    "Val Accuracy": epoch_acc.item(),
+                    **{f"Val {k}": v for k, v in metrics.items() if k != 'confusion_matrix'},
+                    "Epoch": epoch
+                })
 
-    # Специфичность (Specificity)
-                  specificity_class = TN / (TN + FP) if (TN + FP) > 0 else 0
-                  specificity.append(specificity_class)
-              print("\nMetrics per class:")
-              for i, class_name in enumerate(class_names):
-                  print(f"Class: {class_name}")
-                  print(f"  Sensitivity: {sensitivity[i]:.4f}")
-                  print(f"  Specificity: {specificity[i]:.4f}")
 
-# Расчет чувствительности (Recall) и F1-меры для каждого класса
-            if phase == 'val':
-              recall = recall_score(all_labels, all_preds, average='macro', zero_division=1)
-              f1 = f1_score(all_labels, all_preds, average='macro', zero_division=1)
-              print(f'Recall: {recall:.4f}')
-              print(f'F1-score: {f1:.4f}')
-              wandb.log({
-        "Val Loss": epoch_loss,
-        "Val F1": f1,
-        "Val AUC": recall,
-        "Epoch": epoch})
-              
-                # Сохранение модели с лучшей точностью
+                #сохранение модели с лучшей точностью
             if epoch_acc > best_acc:
                     best_acc = epoch_acc
                     best_model_wts = copy.deepcopy(model.state_dict())
-# Сохранение чекпоинта после каждой эпохи
-        checkpoint_path = os.path.join(result_dir, f'checkpoint_epoch_{epoch+1}.ckpt')
-        torch.save(model.state_dict(), checkpoint_path)
-        print(f"Checkpoint saved at {checkpoint_path}")
+
+#сохранение чекпоинта после каждой эпохи
+        save_checkpoint(model, optimizer, scheduler, epoch, checkpoint_dir)
         torch.cuda.empty_cache()
         gc.collect()
-    
-    
-    # Время обучения
+    #время обучения
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
     print('Best val Acc: {:4f}'.format(best_acc))
 
     model.load_state_dict(best_model_wts)
-    # Построение графиков
+    #построение графиков
     plt.figure(figsize=(12, 5))
     plt.subplot(1, 2, 1)
     plt.plot(train_losses, label='Train Loss')
@@ -184,6 +223,8 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
     plt.ylabel('Loss')
     plt.legend()
     plt.title('Loss')
+
+
 
     plt.subplot(1, 2, 2)
     plt.plot(train_accuracies, label='Train Accuracy')
@@ -198,10 +239,9 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=num_epoch):
 
     return model
 
-# Запуск обучения
-model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=num_epoch)
-
-# Сохранение лучшей модели
+#Запуск обучения с продолжением
+model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=num_epoch, start_epoch=start_epoch)
+#Сохранение лучшей модели
 result_dir_s = os.path.join(result_dir, 'best_ensemble_model.ckpt')
 torch.save(model_ft.state_dict(), result_dir_s)
-# wandb.finish()
+wandb.finish()
